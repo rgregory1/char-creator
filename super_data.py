ace521f369e2ae58afded3b24ad70d2111d61bb4
rgregory1 <31772093+rgregory1@users.noreply.github.com>
2017-11-07 16:58:54 -0500

    trial again
    added a few words

--- super_data.py
+++ super_data.py
@@ -5,4 +5,4 @@ arch_brawler = {'archetype': 'Brawler', 'summary' : "You're a close in fighter w
 
 arch_brick = {'archetype': 'Brick', 'summary': "You're the bruising, super-strong fighter on your team...", 'move': 5, 'body_points' : 8, 'psych_points' : 6, 'maj-p': ['Super-Strength'], 'minor_p_list': ['Armor', 'Burrowig', 'Density Increase', 'Leaping', 'Magic Artifact', 'Massive', 'Rage', 'Resistance'], 'min_p_num': 2}
 
-# this is my added change!
+# this is my added change! and another change
